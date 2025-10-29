7a82cba0ed882ab0a08b54a2d1e28f753bce68d7
juliettelavoie <juliette.lavoie@hotmail.ca>
2025-10-29 14:26:31 -0400

    try to pass coverage

--- tests/regression/test_cli_get_regression.py
+++ tests/regression/test_cli_get_regression.py
@@ -14,10 +14,6 @@ from cmipcite.cli import app
 runner = CliRunner()
 
 
-TEST_PID = "hdl:21.14100/f2f502c9-9626-31c6-b016-3f7c0534803b"
-TEST_TRACKING_ID = "hdl:21.14100/be06a059-363d-47a4-97a2-d5253190fd15"
-
-
 @pytest.mark.parametrize(
     "out_path, out_format, author_list_style",
     (
@@ -26,10 +22,10 @@ TEST_TRACKING_ID = "hdl:21.14100/be06a059-363d-47a4-97a2-d5253190fd15"
         pytest.param("tmp.txt", "text", "long", id="outfile-text-longauthors"),
     ),
 )
-def test_citations_from_PID(
+def test_citations_args(
     out_path, out_format, author_list_style, file_regression, tmpdir
 ):
-    args = ["get", TEST_PID]
+    args = ["get", "hdl:21.14100/f2f502c9-9626-31c6-b016-3f7c0534803b"]
 
     if out_path is not None:
         out_path_full = Path(tmpdir) / out_path
@@ -56,40 +52,26 @@ def test_citations_from_PID(
     file_regression.check(res, extension=suffix)
 
 
-# test nearly duplicated to avoid avoid more than 5 args
 @pytest.mark.parametrize(
-    "out_path, out_format, author_list_style",
+    "input_id",
     (
-        pytest.param(None, None, None, id="defaults"),
-        pytest.param("tmp.bib", "bibtex", "short", id="outfile-bibtex-shortauthors"),
-        pytest.param("tmp.txt", "text", "long", id="outfile-text-longauthors"),
+        pytest.param("hdl:21.14100/f2f502c9-9626-31c6-b016-3f7c0534803b", id="pid"),
+        pytest.param(
+            "hdl:21.14100/be06a059-363d-47a4-97a2-d5253190fd15", id="tracking"
+        ),
+        pytest.param(
+            "hdl:21.14100/2e216556-472e-4d88-a414-42b0c0944d36", id="consortium"
+        ),
     ),
 )
-def test_citations_from_tracking(
-    out_path, out_format, author_list_style, file_regression, tmpdir
-):
-    args = ["get", TEST_TRACKING_ID]
-
-    if out_path is not None:
-        out_path_full = Path(tmpdir) / out_path
-        args.extend(["--out-path", str(out_path_full)])
-
-    if out_format is not None:
-        args.extend(["--format", out_format])
-
-    if author_list_style is not None:
-        args.extend(["--author-list-style", author_list_style])
+def test_types_of_id(input_id, file_regression, tmpdir):
+    args = ["get", input_id]
 
     result = runner.invoke(app, args)
 
     assert result.exit_code == 0, result.stdout
 
-    if out_path is None:
-        res = result.stdout
-        suffix = ".txt"
-    else:
-        suffix = out_path_full.suffix
-        with open(out_path_full) as fh:
-            res = fh.read()
+    res = result.stdout
+    suffix = ".txt"
 
     file_regression.check(res, extension=suffix)
